abae9ab52040d7b3133183fdace59f71fd994169
Vladimir Kovpak <cn007b@gmail.com>
2018-05-23 23:32:39 +0300

    Revert "Test 2."
    This reverts commit d5358fb1c0ad0652b6d6daa9a58cbfb82336b3a3.

---
+++ src/go-app/main.go
@@ -0,0 +1,16 @@
+package go_app
+
+import (
+	"net/http"
+	"fmt"
+	"google.golang.org/appengine"
+)
+
+func init() {
+	http.HandleFunc("/", indexHandler)
+	appengine.Main()
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is monitoring.")
+}
