2ed2f1b791ee5666fe9c96ecbd9b053c77e0c6e5
Vladimir Kovpak <cn007b@gmail.com>
2018-05-30 22:19:12 +0300

    Added TrackVisit.

--- src/go-app/main.go
+++ src/go-app/main.go
@@ -1,9 +1,13 @@
 package go_app
 
 import (
-	"net/http"
+	"errors"
 	"fmt"
+	"golang.org/x/net/context"
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+	"net/http"
+	"time"
 )
 
 func init() {
@@ -12,5 +16,28 @@ func init() {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	TrackVisit(ctx)
+
 	fmt.Fprintln(w, "This is monitoring 🖥📈📊📉 .")
+}
+
+const (
+	Name = "Visit"
+)
+
+type Visit struct {
+	TimeStamp time.Time
+}
+
+func TrackVisit(ctx context.Context) (datastore.Key, error) {
+	v := Visit{TimeStamp: time.Now().UTC()}
+	key := datastore.NewIncompleteKey(ctx, Name, nil)
+
+	k, err := datastore.Put(ctx, key, &v)
+	if err != nil {
+		return datastore.Key{}, errors.New("Failed to store visit, error: " + err.Error())
+	}
+
+	return *k, nil
 }
