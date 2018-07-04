27114897045cee841cb7163f790257e7ab4c5078
Vladimir Kovpak <cn007b@gmail.com>
2018-07-04 10:56:40 +0300

    Added experiment/http.

--- src/go-app/controller/experiment/a-route.go
+++ src/go-app/controller/experiment/a-route.go
@@ -5,6 +5,8 @@ import (
 )
 
 func RegisterRoutes() {
+	http.HandleFunc("/experiment/http/error500", httpError500Handler)
+
 	http.HandleFunc("/experiment/stackdriver/errors", stackDriverErrorsHandler)
 	http.HandleFunc("/experiment/stackdriver/logs", stackDriverLogsHandler)
 }
