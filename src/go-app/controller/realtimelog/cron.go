5af005ca8a11eed972c8e27a3afe07ebd54f371a
Vladimir Kovpak <cn007b@gmail.com>
2018-08-13 16:55:52 +0300

    Added common/controller/.

--- src/go-app/controller/realtimelog/cron.go
+++ src/go-app/controller/realtimelog/cron.go
@@ -1,63 +1,38 @@
 package realtimelog
 
 import (
-	"fmt"
 	"google.golang.org/appengine"
-	"google.golang.org/appengine/log"
 	"net/http"
 
+	"go-app/common/controller"
 	"go-app/service/queue"
 	"go-app/service/realtimelog"
 )
 
 func CronTaskPingHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
-
 	res, err := realtimelog.Ping(ctx, "ping-from-cron")
-	if err == nil {
-		w.WriteHeader(http.StatusOK)
-		log.Infof(ctx, "[🤖✅ ] Performed Ping, result: %v", res)
-	} else {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Errorf(ctx, "[🤖❌ ] Filed to perform Ping, error: %v", err)
-	}
+
+	controller.InternalResponse(ctx, w, "CronTaskPing", res, err)
 }
 
 func CronTaskPingingHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
-
 	res, err := realtimelog.Pinging(ctx, "pinging-from-cron")
-	if err == nil {
-		w.WriteHeader(http.StatusOK)
-		log.Infof(ctx, "[🤖✅ ] Performed Pinging, result: %v", res)
-	} else {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Errorf(ctx, "[🤖❌ ] Filed to perform Pinging, error: %v, result: %+v", err, res)
-	}
+
+	controller.InternalResponse(ctx, w, "CronTaskPinging", res, err)
 }
 
 func CronTaskAddPingJobHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
-
 	err := queue.AddPingJob(ctx, "ping-from-queue")
-	if err == nil {
-		w.WriteHeader(http.StatusOK)
-		log.Infof(ctx, "[🤖✅ ] Performed AddPingJob.")
-	} else {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "[🤖❌ ] Filed to perform AddPingJob, error: %v", err)
-	}
+
+	controller.InternalResponse(ctx, w, "CronTaskAddPingJob", "ok", err)
 }
 
 func CronTaskAddPingingJobHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
-
 	err := queue.AddPingingJob(ctx, "pinging-from-queue")
-	if err == nil {
-		w.WriteHeader(http.StatusOK)
-		log.Infof(ctx, "[🤖✅ ] Performed AddPingingJob.")
-	} else {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "[🤖❌ ] Filed to perform AddPingingJob, error: %v", err)
-	}
+
+	controller.InternalResponse(ctx, w, "CronTaskAddPingingJob", "ok", err)
 }
--- src/go-app/controller/realtimelog/worker.go
+++ src/go-app/controller/realtimelog/worker.go
@@ -2,36 +2,24 @@ package realtimelog
 
 import (
 	"google.golang.org/appengine"
-	"google.golang.org/appengine/log"
 	"net/http"
 
+	"go-app/common/controller"
 	"go-app/service/queue"
 )
 
 func WorkerPingHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
-
 	msg := r.FormValue("msg")
 	res, err := queue.PerformPingJob(ctx, msg)
-	if err == nil {
-		w.WriteHeader(http.StatusOK)
-		log.Infof(ctx, "[👾✅ ] Performed PerformPingJob, result: %v", res)
-	} else {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Errorf(ctx, "[👾❌ ] Filed to perform PerformPingJob, error: %v", err)
-	}
+
+	controller.InternalResponse(ctx, w, "WorkerPing", res, err)
 }
 
 func WorkerPingingHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
-
 	msg := r.FormValue("msg")
 	res, err := queue.PerformPingingJob(ctx, msg)
-	if err == nil {
-		w.WriteHeader(http.StatusOK)
-		log.Infof(ctx, "[👾✅ ] Performed PerformPingingJob, result: %v", res)
-	} else {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Errorf(ctx, "[👾❌ ] Filed to perform PerformPingingJob, error: %v, result: %v", err, res)
-	}
+
+	controller.InternalResponse(ctx, w, "WorkerPinging", res, err)
 }
